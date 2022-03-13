92e59f928c84a9b4a23c8830b79b2f939c78ceda
matthew-spire <matthew.thomas.spire@gmail.com>
2022-03-13 08:08:29 -0700

    first commit

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.zybooks.pizzaparty
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -7,9 +9,6 @@ import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.ceil
-
-// Declares and initializes a constant for the number of slices per pizza
-const val SLICES_PER_PIZZA = 8
 
 /**
  * The MainActivity class contains the Kotlin code that interacts with the UI.
@@ -52,32 +51,38 @@ class MainActivity : AppCompatActivity() {
         numAttendEditText = findViewById(R.id.num_attend_edit_text)
         numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
         howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+        supportActionBar!!.setBackgroundDrawable(ColorDrawable(Color.parseColor("#000000")))
     }
 
     /**
-     * The function calculateClick calculates the number of pizzas needed to feed the given number
-     * of people based on how hungry they are. The result is displayed by assigning a new string
-     * using the TextView text setter.
+     * The function calculateClick gets the number of people and how hungry they are, then calls
+     * PizzaCalculator. PizzaCalculator calculates the number of pizzas needed to feed the given
+     * number of people based on how hungry they are. The result is displayed by assigning a new
+     * string using the TextView text setter.
      *
      * @param view View that was clicked, i.e., the button.
      */
     fun calculateClick(view: View) {
 
-        // Get the text that was typed into the EditText
+        // Get the text that was typed into the EditText for the number of people
         val numAttendStr = numAttendEditText.text.toString()
 
         // Convert the text into an integer
-        val numAttend = numAttendStr.toInt()
+        val numAttend = numAttendStr.toIntOrNull() ?: 0
 
-        // Determine how many slices on average each person will eat
-        val slicesPerPerson = when (howHungryRadioGroup.checkedRadioButtonId) {
-            R.id.light_radio_button -> 2
-            R.id.medium_radio_button -> 3
-            else -> 4
+        // Get hunger level selection
+        val hungerLevel = when (howHungryRadioGroup.checkedRadioButtonId) {
+            R.id.light_radio_button -> PizzaCalculator.HungerLevel.LIGHT
+            R.id.medium_radio_button -> PizzaCalculator.HungerLevel.MEDIUM
+            else -> PizzaCalculator.HungerLevel.RAVENOUS
         }
 
-        // Calculate and show the number of pizzas needed
-        val totalPizzas = ceil(numAttend * slicesPerPerson / SLICES_PER_PIZZA.toDouble()).toInt()
-        numPizzasTextView.text = "Total pizzas: $totalPizzas"
+        // Get the number of pizzas needed
+        val calc = PizzaCalculator(numAttend, hungerLevel)
+        val totalPizzas = calc.totalPizzas
+
+        // Place totalPizzas into the string resource and display
+        val totalText = getString(R.string.total_pizzas, totalPizzas)
+        numPizzasTextView.text = totalText
     }
 }
